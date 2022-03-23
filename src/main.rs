fbc04503b8ecf9a666856d8c9fb254fa6d9ab2b4
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2022-03-23 20:15:29 +0100

    Recursively calculate sum of folder

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,27 @@ fn to_kilobytes(length: f64) -> f64 {
     length / 1000_f64
 }
 
+fn recursive_sum(path: String) -> f64 {
+    let mut sum: f64 = 0.00;
+
+    for file in fs::read_dir(path).unwrap() {
+        let unwrap = file.unwrap();
+
+        if unwrap.file_name() == "." || unwrap.file_name() == ".." {
+            continue;
+        }
+
+        let metadata = unwrap.metadata().unwrap();
+        if metadata.file_type().is_dir() {
+            sum += recursive_sum(format!("{}", unwrap.path().display()))
+        } else {
+            sum += metadata.len() as f64;
+        }
+    }
+
+    sum
+}
+
 #[derive(Debug)]
 struct FileFound {
     name: String,
@@ -23,29 +44,36 @@ fn main() {
 
         let metadata = unwrap.metadata().unwrap();
 
+        #[allow(unused_assignments)]
+        let mut file_size = 0.00;
+
         if metadata.file_type().is_dir() {
-            continue;
+            file_size = recursive_sum(format!("{}", unwrap.path().display()));
+
+            files_found.push(FileFound {
+                name: format!("{}/", unwrap.file_name().into_string().unwrap()),
+                weight: to_kilobytes(file_size),
+            })
+        } else {
+            file_size = metadata.len() as f64;
+            files_found.push(FileFound {
+                name: unwrap.file_name().into_string().unwrap(),
+                weight: to_kilobytes(file_size),
+            })
         }
-
-        let file_size = metadata.len() as f64;
-
-        files_found.push(FileFound {
-            name: format!("{}", unwrap.path().display()),
-            weight: to_kilobytes(file_size),
-        })
     }
 
     files_found.sort_by(|a, b| b.weight.partial_cmp(&a.weight).unwrap());
 
     for file in files_found {
         if file.weight <= 10.0 {
-            println!("{} ({})", file.name, format!("{} kb", file.weight).green())
+            println!("{} ({})", file.name, format!("{}Kb", file.weight).green())
         } else if file.weight <= 1000.0 {
-            println!("{} ({})", file.name, format!("{} kb", file.weight).yellow())
+            println!("{} ({})", file.name, format!("{}Kb", file.weight).yellow())
         } else if file.weight > 1000.0 {
-            println!("{} ({})", file.name, format!("{} kb", file.weight).red())
+            println!("{} ({})", file.name, format!("{}Kb", file.weight).red())
         } else {
-            println!("{} ({})", file.name, format!("{} kb", file.weight).cyan())
+            println!("{} ({})", file.name, format!("{}Kb", file.weight).cyan())
         }
     }
 }
